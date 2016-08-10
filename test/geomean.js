4a9814c170bfbe12a0d73b357adf28a1b30be748
Gareth Ellis <gareth@gsellis.com>
2016-08-10 20:27:08 +0100

    better discription

--- test/geomean.js
+++ test/geomean.js
@@ -10,11 +10,11 @@ describe('#geomean', function() {
 		a = [1, 2, 3, 4];
 		expect(geomean.geomean(a)).to.equal(2.2133638394006434);
 	});
-	it('checks we get a null if we pass one or less items in array',function() {
+	it('checks we get a the correct answer if we include a 0 (we want it ignored from the geomean calc)',function() {
 		a = [1, 2, 3, 0];
 		expect(geomean.geomean(a)).to.equal(1.8171205928321397);
 	});
-	it('checks we get a null if we pass one or less items in array',function() {
+	it('checks we get a the correct answer as above if we don't include a 0 (we want it to be same answer as above)',function() {
 		a = [1, 2, 3];
 		expect(geomean.geomean(a)).to.equal(1.8171205928321397);
 	});
